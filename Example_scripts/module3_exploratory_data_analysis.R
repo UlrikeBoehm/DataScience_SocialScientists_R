41e077ce9a46aa9c81afe3c7898b8d897515c33c
Ulrike Boehm <ulrike.boehm@gmail.com>
2020-09-22 23:47:41 -0400

    updated r code

--- Example_scripts/module3_exploratory_data_analysis.R
+++ Example_scripts/module3_exploratory_data_analysis.R
@@ -1,7 +1,7 @@
 # set the library with the packages we use
 library(gglot2)
 library(tidyverse)
-library(cowplot)
+library(cowplot) # for grids 
 
 # load in the data
 bihar_data <- read_csv("data/Bihar_sample_data.csv")
@@ -15,7 +15,7 @@ bihar_adult_females <- filter(bihar_data, adult==1, female==1)
 # have a look 
 print(bihar_adult_females)
 
-# dafault histogram in ggplot
+# default histogram in ggplot
 ggplot(bihar_adult_females, aes(height_cm)) + geom_histogram()
 ggsave("output/bihar_raw.pdf")
 
@@ -24,6 +24,7 @@ bihar_adult_females_trunc <- filter(bihar_adult_females, height_cm>120, height_c
 
 # Plotting again, with a nicer label, and some color
 ggplot(bihar_adult_females_trunc, aes(height_cm)) +
-  geom_histogram(fill="blue", color="darkblue") +
+  geom_histogram(fill="blue", color="darkblue", aes(height_cm, ..density..)) +
+  geom_density(kernel="gaussian",aes(height_cm)) +
   xlab("Height in centimeters, Bihar Females")
 ggsave("output/bihar_better.pdf")
